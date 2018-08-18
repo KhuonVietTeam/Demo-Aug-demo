74fc9feb3b75b5bffb4a6275063567585de722e5
Tuan Nhan <tuannhanle@gmail.com>
2018-08-18 15:40:42 +0700

    NHAN: Create Event Manager Script

---
+++ Assets/Fish-shooting-Aug/Scripts/Manager/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static BulletSeller bulletseller;
+    public static CoinBuyer coinbuyer;
+    public static CoinSeller coinseller;
+    public static FishBuyer fishbuyer;
+    // Use this for initialization
+    private void Awake()
+    {
+        bulletseller = new BulletSeller();
+        coinbuyer = new CoinBuyer();
+        coinseller = new CoinSeller();
+        fishbuyer = new FishBuyer();
+    }
+}
